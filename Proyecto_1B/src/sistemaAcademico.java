a77557024df89cc283cb1e36a3c37198e7b99553
LeonardPM <leonardpaccham@gmail.com>
2024-11-20 01:13:15 -0500

    tabla y ajustes de variables sin usar

--- Proyecto_1B/src/sistemaAcademico.java
+++ Proyecto_1B/src/sistemaAcademico.java
@@ -1,14 +1,36 @@
+/*
+ * 2. Sistema académico de la UTPL
+ * La UTPL, le contrata para realizar una aplicación pequeña que permita la gestión de
+ * calificaciones de los estudiantes en una materia.
+ * Del Estudiante se registra su: nombre, cédula y la materia a cruzar. (Auto genere estos datos
+ * con la función ALEATORIO/RAMDOM)
+ * Se debe verificar la aprobación de una materia dadas tres calificaciones: ACD (3.5/10), APE
+ * (3.5/10) y AA (3/10). Se aprueba si la sumatoria es de al menos 70%, si cumple con este
+ * requisito, se considera que ha aprobado, caso contrario informar al estudiante que deberá rendir
+ * un examen de recuperación sobre 3.5/10 pts. agregado al 60% acumulado de los componentes
+ * ACD, APE y AA.
+ * Ahora, debe implementar los siguientes requisitos en su programa:
+ * 1. El programa debe permitir al usuario ingresar los datos de un estudiante, incluyendo su
+ * nombre y cédula. (Nota: realizarlo por medio de simulación – random/aleatorio en el main())
+ * 2. El programa debe permitir auto generar simulación de los datos de una materia, incluyendo
+ * su nombre y las calificaciones del estudiante en las categorías ACD, APE y AA.
+ * 3. El programa debe verificar si un estudiante ha aprobado la materia. Para ello, se deben
+ * evaluar las calificaciones del estudiante en las tres categorías (ACD, APE y AA).
+ * 4. Al finalizar, el programa debe mostrar las estadísticas/porcentajes de aprobación y/o
+ * reprobación de la materia de un X curso.
+ */
+
 import java.util.Random;
 
 public class sistemaAcademico {
     public static void main(String[] args) {
         float acd, ape, aa, promedio;
         int course, id, nStudent = 9;
-        String name;
+        String name, nApprovedC1, nApprovedC2, nApprovedC3;
         Random rand = new Random();
-        int course1Count = 0, course2Count = 0, course3Count = 0, nApprovedC1 = 0, nFailedC1 = 0, nApprovedC2 = 0, nFailedC2 = 0, nApprovedC3 = 0, nFailedC3 = 0;
 
-
+        System.out.printf("%s\t%s\t\t\t%s\t%s\t\t\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+                "|", "Nombre", "|", "id", "|", "curso", "|", "Promedio", "|", "Aprueba/Reprueba", "|");
         for (int i = 0; i < nStudent; i++) {
             id = 1000000000 + rand.nextInt(900000000);
             name = "Estudiante" + rand.nextInt(nStudent);
@@ -19,46 +41,47 @@ public class sistemaAcademico {
             promedio = acd + ape + aa;
 
             if (course == 1) {
-                course1Count++;
-                System.out.println("-------------------");
-                System.out.println("Name:   " + name);
-                System.out.println("Id:     " + id);
-                //                System.out.println("Course: " + course);
-                //                System.out.println("acd:    " + acd);
-                //                System.out.println("ape:    " + ape);
-                //                System.out.println("aa:     " + aa);
-                System.out.println("promedio: " + promedio);
 
                 if (promedio >= 7)
-                    nApprovedC1++;
+                    nApprovedC1 = "aprueba ";
                 else {
-                    nFailedC1++;
+                    nApprovedC1 = "reprueba";
                 }
 
-
+                System.out.printf("%s\t%s\t\t%s\t%d\t%s\t%d\t\t%s\t%.3f\t\t%s\t\t%s\t\t%s\n", "|", name, "|", id, "|",
+                        course, "|", promedio, "|", nApprovedC1, "|");
             } else if (course == 2) {
 
-                course2Count++;
                 if (promedio >= 7)
-                    nApprovedC2++;
+                    nApprovedC2= "aprueba ";
                 else {
-                    nFailedC2++;
+                    nApprovedC2= "reprueba";
                 }
+                System.out.printf("%s\t%s\t\t%s\t%d\t%s\t%d\t\t%s\t%.3f\t\t%s\t\t%s\t\t%s\n", "|", name, "|", id, "|",
+                        course, "|", promedio, "|", nApprovedC2, "|");
             } else {
-                course3Count++;
                 if (promedio >= 7)
-                    nApprovedC3++;
+                    nApprovedC3="aprueba ";
                 else {
-                    nFailedC3++;
+                    nApprovedC3="reprueba";
                 }
+                System.out.printf("%s\t%s\t\t%s\t%d\t%s\t%d\t\t%s\t%.3f\t\t%s\t\t%s\t\t%s\n", "|", name, "|", id, "|",
+                        course, "|", promedio, "|", nApprovedC3, "|");
             }
-
 
         }
 
-        System.out.println("--------------------------------------");
-        System.out.println("Total de estudiantes en el curso 1: " + course1Count + " | " + nApprovedC1 + " | " + nFailedC1);
-        System.out.println("Total de estudiantes en el curso 2: " + course2Count + " | " + nApprovedC2 + " | " + nFailedC2);
-        System.out.println("Total de estudiantes en el curso 3: " + course3Count + " | " + nApprovedC3 + " | " + nFailedC3);
     }
 }
+/*
+ * |	Nombre			|	id			|	curso	|	Promedio	|	Aprueba/Reprueba	|
+ * |	Estudiante3		|	1277425531	|	2		|	5.149		|		reprueba		|
+ * |	Estudiante3		|	1677486026	|	2		|	3.257		|		reprueba		|
+ * |	Estudiante4		|	1254930284	|	1		|	7.817		|		aprueba 		|
+ * |	Estudiante7		|	1498466353	|	1		|	6.167		|		reprueba		|
+ * |	Estudiante8		|	1051538470	|	2		|	4.752		|		reprueba		|
+ * |	Estudiante8		|	1306598593	|	3		|	3.109		|		reprueba		|
+ * |	Estudiante2		|	1474777809	|	1		|	6.024		|		reprueba		|
+ * |	Estudiante5		|	1485329311	|	2		|	5.000		|		reprueba		|
+ * |	Estudiante0		|	1134377411	|	3		|	3.774		|		reprueba		|
+ */
